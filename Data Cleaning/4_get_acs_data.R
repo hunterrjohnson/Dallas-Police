a172db6ff72c5104acc2cf8ecb18e40b2fb611b6
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2021-04-29 18:40:10 -0700

    Update 4_get_acs_data.R
    Updates file to get ACS data for counties adjacent to Dallas County.

--- Data Cleaning/4_get_acs_data.R
+++ Data Cleaning/4_get_acs_data.R
@@ -1,6 +1,6 @@
 # Get ACS Data
 
-lapply(c('tigris','acs','sf','raster'), library, character.only = TRUE)
+lapply(c('tigris','acs','sf','raster','dplyr','data.table'), library, character.only = TRUE)
 
 '%!in%' <- function(x,y){
   !('%in%'(x,y))
@@ -23,22 +23,37 @@ calls <- calls[which(!is.na(calls$latitude)),]
 # Write calls
 readr::write_csv(calls, '1_Data/1_Raw/Dallas_Blockgroups/calls_latlons.csv')
 
-api.key.install('a96bec964e4ba4c500e72bb55ce34cb098714c23') # Census API key
-dallas_geo <- geo.make(state = 'TX', county = 'Dallas', tract = '*', block.group = '*') # geo.make required to retrieve data
+get_raw_acs <- function(county_num, county_name) {
+  
+  api.key.install('a96bec964e4ba4c500e72bb55ce34cb098714c23') # Census API key
+  
+  dat_geo <- geo.make(state = 'TX', county = county_num, tract = '*', block.group = '*')
+  dat_bgdata <- acs.fetch(endyear = 2015, span = 5, geography = dat_geo, table.number = c('B02001','B03002','B19013','B15003'))
+  dat <- data.frame(dat_bgdata@estimate)
+  dat <- dat %>% mutate(state = dat_bgdata@geography$state,
+                        county = dat_bgdata@geography$county,
+                        tract = dat_bgdata@geography$tract,
+                        blockgroup = dat_bgdata@geography$blockgroup)
+  # Write raw data to csv
+  data.table::fwrite(dat, file = paste0('1_Data/1_Raw/Dallas_Blockgroups/County_Data/',county_name,'_bg_acs_raw.csv'))
+  
+  # Assign to environment
+  assign(paste0(county_name,'_dat'), dat, envir=.GlobalEnv)
+  
+}
 
-# Retrieve block group data for Dallas County for 2015 from ACS 5-year survey
-# - B02001: Race table
-# - B03002: Hispanic/Latino table
-# - B19013: Median household income
-# - B15003: Educational attainment
-dallas_bgdata <- acs.fetch(endyear = 2015, span = 5, geography = dallas_geo, table.number = c('B02001','B03002','B19013','B15003'))
+# Get Dallas and all adjacent counties
+get_raw_acs(county_num = 113, county_name = 'dallas')
+get_raw_acs(county_num = 85, county_name = 'collin')
+get_raw_acs(county_num = 397, county_name = 'rockwall')
+get_raw_acs(county_num = 257, county_name = 'kaufman')
+get_raw_acs(county_num = 139, county_name = 'ellis')
+get_raw_acs(county_num = 251, county_name = 'johnson')
+get_raw_acs(county_num = 439, county_name = 'tarrant')
+get_raw_acs(county_num = 121, county_name = 'denton')
 
-# Clean up data
-dat <- data.frame(dallas_bgdata@estimate)
-dat <- dat %>% mutate(state = dallas_bgdata@geography$state,
-                      county = dallas_bgdata@geography$county,
-                      tract = dallas_bgdata@geography$tract,
-                      blockgroup = dallas_bgdata@geography$blockgroup)
+dat <- rbind(dallas_dat, collin_dat, rockwall_dat, kaufman_dat, ellis_dat, johnson_dat, tarrant_dat, denton_dat)
+rm(list=setdiff(ls(), c('dat','calls')))
 
 # Write raw data to csv
 readr::write_csv(dat, '1_Data/1_Raw/Dallas_Blockgroups/dallas_bg_acs_raw.csv')
@@ -65,7 +80,19 @@ summary(dat$lessthanhs)
 dat <- dat[, -which(grepl('_', colnames(dat)))]
 
 # Get shapefile for Dallas block groups
-dallas_shp <- tigris::block_groups(state = 'Texas', county = 'Dallas', year = 2015)
+dallas_shp <- tigris::block_groups(state = 'Texas', county = 113, year = 2015)
+collin_shp <- tigris::block_groups(state = 'Texas', county = 85, year = 2015)
+rockwall_shp <- tigris::block_groups(state = 'Texas', county = 397, year = 2015)
+kaufman_shp <- tigris::block_groups(state = 'Texas', county = 257, year = 2015)
+ellis_shp <- tigris::block_groups(state = 'Texas', county = 139, year = 2015)
+johnson_shp <- tigris::block_groups(state = 'Texas', county = 251, year = 2015)
+tarrant_shp <- tigris::block_groups(state = 'Texas', county = 439, year = 2015)
+denton_shp <- tigris::block_groups(state = 'Texas', county = 121, year = 2015)
+dallas_shp <- rbind(dallas_shp, collin_shp, rockwall_shp, kaufman_shp,
+                    ellis_shp, johnson_shp, tarrant_shp, denton_shp)
+rm(list=setdiff(ls(), c('dallas_shp','dat','calls')))
+
+# Need to make sure tracts in both data sets can merge
 summary(nchar(dallas_shp$TRACTCE))
 summary(nchar(dat$tract))
 
@@ -82,6 +109,7 @@ length(which(dat$tract %in% dallas_shp$TRACTCE))
 
 # Join geometry to df
 dallas_shp <- st_as_sf(dallas_shp)
+dat$county <- ifelse(dat$county==85, '085', as.character(dat$county))
 dat$GEOID <- paste0(dat$state, dat$county, dat$tract, dat$blockgroup) # Get GEOID variable in common for merge
 length(which(dat$GEOID %in% dallas_shp$GEOID))
 df <- left_join(dat, dallas_shp[, c('GEOID')], by = 'GEOID')
@@ -95,10 +123,10 @@ df <- df %>% mutate(bg_prop_white = white / (white + black + hispanic + other),
 # Reorder columns
 df <- df[,c(11,6,7,8,9,1,2,3,4,5,10,13,14,15,16,12)]
 
-rm(dallas_bgdata, dallas_geo, dallas_shp, dat)
+rm(list=setdiff(ls(), c('df','calls')))
 
-# Convert to sf file
-calls <- st_as_sf(calls, coords = c('longitude','latitude'), remove = FALSE, crs = 4326) # Get coordinates for spatial join
+# Get coordinates for spatial join
+calls <- st_as_sf(calls, coords = c('longitude','latitude'), remove = FALSE, crs = 4326)
 
 # Merge blockgroup data to calls by spatial join
 calls <- st_join(calls, left=TRUE, st_as_sf(df, crs=4326))
@@ -110,11 +138,11 @@ calls$geometry <- NULL
 
 # Assume calls assigned to multiple blockgroups are in most populous one
 calls$pop <- calls$white+calls$black+calls$hispanic+calls$other # population total
-calls <- calls %>% group_by(inc_id) %>% mutate(maxpop = max(pop)) %>% ungroup()
+calls <- calls %>% group_by(dispatchnum) %>% mutate(maxpop = max(pop)) %>% ungroup()
 calls <- calls[which(calls$maxpop==calls$pop),]
 
 # Write clean block group data
-haven::write_dta(calls, '2-stata_data/DPD_blockgroups.dta')
+data.table::fwrite(calls, '1_Data/2_Clean/blockgroups.csv')
 
 
 
