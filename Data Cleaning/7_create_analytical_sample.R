3b01fccbfe01af3302f763554adffd197ae6dfdb
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2021-04-29 21:31:52 -0700

    Update and rename 5_merge_and_collapse.R to 7_create_analytical_sample.R

--- Data Cleaning/7_create_analytical_sample.R
+++ Data Cleaning/7_create_analytical_sample.R
@@ -2,6 +2,10 @@
 
 lapply(c('data.table','dplyr'), library, character.only = TRUE)
 
+'%!in%' <- function(x,y){
+  !('%in%'(x,y))
+}
+
 setwd('/Users/hunterjohnson/Dropbox/Dallas Projects')
 
 #===============================================================================
@@ -12,9 +16,6 @@ dispatch$V1 <- NULL
 
 # Merge blockgroup data
 blockgroups <- fread('1_Data/2_Clean/blockgroups.csv')
-blockgroups$pop <- blockgroups$white+blockgroups$black+blockgroups$hispanic+blockgroups$other # population total
-blockgroups <- blockgroups %>% group_by(dispatchnum) %>% mutate(maxpop = max(pop)) %>% ungroup()
-blockgroups <- blockgroups[which(blockgroups$maxpop==blockgroups$pop),] # assume calls w/ multiple blockgroups are in most populous one
 df <- left_join(dispatch, blockgroups[, c('dispatchnum','GEOID','medhhinc',
                                           'lessthanhs','bg_prop_white','bg_prop_black',
                                           'bg_prop_hisp','bg_prop_other')], by='dispatchnum')
@@ -30,7 +31,17 @@ force$force_used <- 1
 force <- unique(force[, c('incidentnum','force_used')]) # keep only an indicator for use of force
 df <- left_join(df, force, by='incidentnum')
 
-rm(dispatch, blockgroups, arrest, force)
+# Merge officer personnel data
+officers <- fread('1_Data/2_Clean/officers.csv')
+officers$badge <- as.character(officers$badge)
+df <- left_join(df, officers, by='badge')
+
+# Merge availability instrument
+avail <- fread('1_Data/2_Clean/availability.csv')
+df <- left_join(df, avail[, c(grep('dispatchnum', colnames(avail)),
+                              grep('avail_rate', colnames(avail))), with=F], by='dispatchnum')
+
+rm(avail, dispatch, blockgroups, arrest, force, officers)
 
 # Define remaining features
 # - all variables should be call level (i.e. dispatchnum is unique ID)
@@ -39,25 +50,14 @@ df$n_arrested <- ifelse(is.na(df$n_arrested), 0, df$n_arrested)
 df$force_used <- ifelse(is.na(df$force_used), 0, df$force_used)
 df <- df %>% group_by(dispatchnum) %>% mutate(reported_inc = max(reported_inc),
                                               n_arrested = max(n_arrested),
-                                              md_dispatch = mean(md_dispatch),
-                                              off_initiated = mean(off_initiated),
-                                              markout = mean(markout),
+                                              arrest = ifelse(n_arrested>0, 1, 0),
                                               force_used = max(force_used)) %>% ungroup()
 
-# Collapse to call level
-keep_cols <- c('dispatchnum','reported_inc','date','year','month','day','hour','problem','priority',
-               'incident_type','off_initiated','markout','division','ra','sector','beat','latitude',
-               'longitude','shelter_loc','loc_freq','n_units','n_offs','called_min','md_dispatch',
-               'medhhinc','lessthanhs','bg_prop_white','bg_prop_black','bg_prop_hisp',
-               'bg_prop_other','n_arrested','force_used')
-df <- unique(df[, colnames(df) %in% keep_cols])
-df <- df[, c(1,32,2:31)] # Move last column to second
-
-# Check that level of observation is dispatchnum
-uniqueN(df$dispatchnum) == nrow(df)
+# Reorder columns
+df <- df[, c(1:24,42:48,25:26,51:52,27:41,53:59,60:61,49:50)]
 
 # Write data
-readr::write_csv(df, file = '1_Data/2_Clean/analysis.csv')
+fwrite(df, '1_Data/2_Clean/analysis.csv')
 
 
 
