da0ee539152bcfc3b54bc86932189f98bba9162c
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2021-04-29 20:37:38 -0700

    Update and rename availability_instrument.R to 6_create_availability_instrument.R

--- Data Cleaning/6_create_availability_instrument.R
+++ Data Cleaning/6_create_availability_instrument.R
@@ -14,29 +14,31 @@ setwd('/Users/hunterjohnson/Dropbox/Dallas Projects/')
 # READ AND MODIFY ORIGINAL DATA FRAME
 
 # Read calls for service data
-df_orig <- haven::read_dta('1_Data/2_Clean/sample_for_availability.dta')
-
-# Drop if inc_id is missing
-df_orig <- df_orig[which(df_orig$inc_id != ''), ]
+df_orig <- fread('1_Data/2_Clean/dispatch.csv')
+df_orig$V1 <- NULL
 
 # Read officer demographic data
-setwd('/Users/hunterjohnson/Dropbox/Dallas Police CJL/')
-officers <- haven::read_dta('2-stata_data/DPD_officer_personel.dta')
-setwd('/Users/hunterjohnson/Dropbox/Dallas Projects/')
-df_orig <- left_join(df_orig, officers[,c('cfs_off_badge','off_gender_fem','off_race_wht',
-                                          'off_race_blk','off_race_hsp','off_race_oth')], by='cfs_off_badge')
+officers <- fread('1_Data/2_Clean/officers.csv')
+officers$badge <- as.character(officers$badge)
+officers$off_race_blk <- ifelse(officers$off_race=='BLACK',1,0)
+officers$off_race_hsp <- ifelse(officers$off_race=='HISPANIC',1,0)
+officers$off_race_wht <- ifelse(officers$off_race=='WHITE',1,0)
+officers$off_race_oth <- ifelse(officers$off_race=='OTHER',1,0)
+officers$off_gender_fem <- ifelse(officers$off_sex=='FEMALE',1,0)
+df_orig <- left_join(df_orig, officers, by='badge')
+rm(officers)
 
 # Create shift variable based on date, watch, division
-df_orig <- df_orig %>% mutate(shift = group_indices(., response_date, watch, cfs_division))
-
-# Break down one year at a time due to vector memory error
-df_orig$cfs_year <- substr(df_orig$response_date, 1, 4)
+df_orig$watch <- ifelse(df_orig$hour %in% c(0:7), 1, NA)
+df_orig$watch <- ifelse(df_orig$hour %in% c(8:15), 2, df_orig$watch)
+df_orig$watch <- ifelse(df_orig$hour %in% c(16:23), 3, df_orig$watch)
+df_orig <- df_orig %>% mutate(shift = group_indices(., date, watch, division))
 
 #===============================================================================
 # CALCULATE AVAILABILITY FOR EACH YEAR INDIVIDUALLY
 
-for (year in c(2009:2019)) {
-  df <- subset(df_orig, df_orig$cfs_year == year)
+for (yr in c(2014:2018)) {
+  df <- subset(df_orig, df_orig$year == yr)
   
   # Count officers by shift by sex
   setDT(df)[, c('n_shift_male','n_shift_female') :=
@@ -45,9 +47,9 @@ for (year in c(2009:2019)) {
   
   # Count unavailable officers by call by sex
   df[, c('n_unavail_male','n_unavail_female') :=
-       df[df, on=.(shift, calledmin<=calledmin, clearedmin>=calledmin), allow.cartesian=TRUE,
-          by=.EACHI, .(sum(x.off_gender_fem[x.inc_id != i.inc_id]==0, na.rm=T),
-                       sum(x.off_gender_fem[x.inc_id != i.inc_id]==1, na.rm=T))][,(1L:3L) := NULL]]
+       df[df, on=.(shift, called_min<=called_min, cleared_min>=called_min), allow.cartesian=TRUE,
+          by=.EACHI, .(sum(x.off_gender_fem[x.dispatchnum != i.dispatchnum]==0, na.rm=T),
+                       sum(x.off_gender_fem[x.dispatchnum != i.dispatchnum]==1, na.rm=T))][,(1L:3L) := NULL]]
   df$n_unavail <- df$n_unavail_female + df$n_unavail_male
   
   # Count officers by shift by race
@@ -58,11 +60,11 @@ for (year in c(2009:2019)) {
   
   # Count unavailable officers by call by race
   df[, c('n_unavail_blk','n_unavail_wht','n_unavail_hsp','n_unavail_oth') :=
-       df[df, on=.(shift, calledmin<=calledmin, clearedmin>=calledmin), allow.cartesian=TRUE,
-          by=.EACHI, .(sum(x.off_race_blk[x.inc_id != i.inc_id]==1, na.rm=T),
-                       sum(x.off_race_wht[x.inc_id != i.inc_id]==1, na.rm=T),
-                       sum(x.off_race_hsp[x.inc_id != i.inc_id]==1, na.rm=T),
-                       sum(x.off_race_oth[x.inc_id != i.inc_id]==1, na.rm=T))][,(1L:3L) := NULL]]
+       df[df, on=.(shift, called_min<=called_min, cleared_min>=called_min), allow.cartesian=TRUE,
+          by=.EACHI, .(sum(x.off_race_blk[x.dispatchnum != i.dispatchnum]==1, na.rm=T),
+                       sum(x.off_race_wht[x.dispatchnum != i.dispatchnum]==1, na.rm=T),
+                       sum(x.off_race_hsp[x.dispatchnum != i.dispatchnum]==1, na.rm=T),
+                       sum(x.off_race_oth[x.dispatchnum != i.dispatchnum]==1, na.rm=T))][,(1L:3L) := NULL]]
   
   # Count available officers
   df <- df %>% mutate(n_avail = (n_shift - n_unavail_male - n_unavail_female),
@@ -91,28 +93,26 @@ for (year in c(2009:2019)) {
   summary(df$avail_rate_oth, na.rm = T)
   
   # Subset to desired columns
-  df <- unique(df[, c(grep('inc_id', colnames(df)),
+  df <- unique(df[, c(grep('dispatchnum', colnames(df)),
                       grep('shift', colnames(df)),
                       grep('avail', colnames(df))), with=F])
-  print(paste0('Number of Duplicates - ', year, ':', sum(duplicated(df$inc_id))))
+  print(paste0('Number of Duplicates - ', yr, ':', sum(duplicated(df$dispatchnum))))
   
   # Write csv
-  readr::write_csv(df, paste0('1_Data/2_Clean/Availability/',year,'_availability.csv'))
-
+  readr::write_csv(df, paste0('1_Data/2_Clean/Availability/',yr,'_availability.csv'))
+  
 }
 
-df$duplicate <- ifelse(duplicated(df$inc_id), 1, 0)
-
 #===============================================================================
-# MERGE INDIVIDUAL YEARS TOGETHER (NOT 2019 DUE TO DUPLICATES)
+# JOIN INDIVIDUAL YEARS TOGETHER
 
 list.files()
 setwd('/Users/hunterjohnson/Dropbox/Dallas Projects/1_Data/2_Clean/Availability/')
 df_avail <- do.call(rbind, lapply(list.files(), data.table::fread))
-uniqueN(df_avail$inc_id) == nrow(df_avail) # Check that there are no duplicated inc_ids
+uniqueN(df_avail$dispatchnum) == nrow(df_avail) # Check that there are no duplicated inc_ids
 
 # Write dta
-haven::write_dta(df_avail, 'DPD_availability.dta')
+data.table::fwrite(df_avail, 'availability.csv')
 
 
 
